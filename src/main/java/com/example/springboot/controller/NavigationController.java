9595e1e12b33d83da74aac11a655443bf36102de
tom314131 <45948148+tom314131@users.noreply.github.com>
2020-12-14 22:32:15 +0200

    Adding filtering and review table in UI Adding ability to send object from controller to html

--- src/main/java/com/example/springboot/controller/NavigationController.java
+++ src/main/java/com/example/springboot/controller/NavigationController.java
@@ -4,16 +4,17 @@ package com.example.springboot.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class NavigationController {
 
-    @GetMapping("/info")
+    /*@GetMapping("/info")
     public String getInfo(Model model){
 //        an attribute used for the navigation bar to determine which tab is currently active
         model.addAttribute("activePage","info");
         return "info";
-    }
+    }*/
 
     @GetMapping("/contact")
     public String getContact(Model model){
