2630558b67497394ed22af53d3be78e0af3d90b2
tom314131 <45948148+tom314131@users.noreply.github.com>
2020-12-19 18:46:38 +0200

    Add all complicated queries and ability to add review

---
+++ src/main/java/com/example/springboot/controller/EditReviewController.java
@@ -0,0 +1,71 @@
+package com.example.springboot.controller;
+
+import com.example.springboot.model.City;
+import com.example.springboot.model.Country;
+import com.example.springboot.model.Hotel;
+import com.example.springboot.model.Review;
+import com.example.springboot.repository.CityRepository;
+import com.example.springboot.repository.CountryRepository;
+import com.example.springboot.repository.HotelRepository;
+import com.example.springboot.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.sql.Date;
+import java.util.List;
+
+@Controller // This means that this class is a Controller
+public class EditReviewController {
+    @Autowired
+    private CountryRepository countryRepository;
+    @Autowired
+    private CityRepository cityRepository;
+    @Autowired
+    private HotelRepository hotelRepository;
+    @Autowired
+    private ReviewRepository reviewRepository;
+/*
+
+    @GetMapping("/editReview")
+    public String editReview(Model model) {
+        model.addAttribute("activePage","addReview");
+        Country country = new Country();
+        model.addAttribute("country", country);
+        List<Country> countryListWithHotels = (List<Country>) countryRepository.allCountriesWithReviews();
+        //List<Country> countryList = (List<Country>) countryRepository.findAll();
+        model.addAttribute("countryListWithHotels", countryListWithHotels);
+        City city = new City();
+        model.addAttribute("city", city);
+
+        List<City> cityList = (List<City>) cityRepository.findAll();
+        model.addAttribute("cityList", cityList);
+        Hotel hotel = new Hotel();
+        model.addAttribute("hotel", hotel);
+
+        List<Hotel> hotelList = (List<Hotel>) hotelRepository.findAll();
+        model.addAttribute("hotelList", hotelList);
+
+        List<Country> addCountryList = (List<Country>) countryRepository.findAll();
+        //List<Country> countryList = (List<Country>) countryRepository.findAll();
+        model.addAttribute("allCountryList", addCountryList);
+
+        Review review = new Review();
+        model.addAttribute("review", review);
+        return "addReview";
+    }
+    @PostMapping("/addReviewSubmitted")
+    public String chooseCountry(@ModelAttribute Review review, Model model) {
+        List<Review> reviews = (List<Review>)reviewRepository.findAll();
+        int id = reviews.get(reviews.size()-1).getId();
+        review.setId(id+1);
+        Date date = new Date(System.currentTimeMillis());
+        review.setDate(date);
+        review.setGuests_composition(review.getGuests_composition().replace('_',' '));
+        reviewRepository.save(review);
+        return "reviewSubmitted";
+    }*/
+}
--- src/main/java/com/example/springboot/repository/CountryRepository.java
+++ src/main/java/com/example/springboot/repository/CountryRepository.java
@@ -1,4 +1,6 @@
 package com.example.springboot.repository;
+import com.example.springboot.model.City;
+import com.example.springboot.model.Hotel;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import com.example.springboot.model.Country;
@@ -7,12 +9,13 @@ import java.util.Collection;
 
 public interface CountryRepository extends CrudRepository<Country, Integer>{
     @Query(
-            value = "SELECT id,name FROM country",
+            value = "SELECT * FROM country LEFT JOIN city ON country.id = country_id WHERE city.country_id is not NULL ",
             nativeQuery = true)
-    Collection<Country> allCountries();
+    Collection<Country> allCountriesWithReviews();
 
     @Query(
             value = "SELECT name FROM country",
             nativeQuery = true)
     Collection<String> getAllCountries();
+
 }
